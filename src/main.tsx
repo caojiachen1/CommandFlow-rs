4f334776b2eea85d634f7e823b638a6d6cb6686d
caojiachen1 <caojiachen1@163.com>
2026-02-26 14:41:16 +0800

    Disable default context menu in the application

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,8 @@ import './index.css'
 import '@xyflow/react/dist/style.css'
 import App from './App.tsx'
 
+document.addEventListener('contextmenu', (event) => {
+  event.preventDefault()
+})
+
 createRoot(document.getElementById('root')!).render(<App />)
