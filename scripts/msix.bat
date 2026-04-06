bee9c70c9af6ab21ddbd4e8a1a4984ad265f1834
caojiachen1 <caojiachen1@163.com>
2026-04-06 18:50:28 +0800

    change title

--- scripts/msix.bat
+++ scripts/msix.bat
@@ -1,8 +1,18 @@
+if not exist .\src-tauri\target\release\commandflow-rs.exe (
+    echo Executable not found. Running build...
+    npm run tauri build
+)
+
+if not exist devcert.pfx (
+    echo Certificate not found. Generating and installing...
+    winapp cert generate
+    winapp cert install .\devcert.pfx
+)
+
+if exist msix rm -rf msix
 mkdir msix
 copy .\src-tauri\target\release\commandflow-rs.exe .\msix
 copy .\appxmanifest.xml .\msix
 xcopy /E /I .\icons .\msix\icons
-winapp cert generate --if-exists skip
 winapp pack .\msix --cert .\devcert.pfx
-@REM sudo winapp cert install .\devcert.pfx
 rm -rf .\msix
